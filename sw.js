5a40e5e1eac6b43e93137415b2a0af6d7e862b55
sam-ro-27 <srowley@byupathway.edu>
2026-08-17 09:49:24 -0500

    creat sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = "aion-ai-v1";
+const FILES = ["./", "./index.html", "./main.js", "./manifest.json"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(r => r || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response; }))));
